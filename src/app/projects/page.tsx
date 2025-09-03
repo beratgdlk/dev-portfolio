c46ab3cc0b72d443dc503ab81f66d873fbf72bdf
Berat Güdelek <beratgdlk@gmail.com>
2025-09-03 03:22:37 +0300

    SVG files have been deleted and the logo background has been updated. In addition, new translations for page titles were added and the profile photo path was updated.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,2 +1,8 @@
+import PageTitle from "@/components/PageTitle";
+export const metadata = {
+  title: "Projects-beratgdlk",
+};
 export { default } from "@/features/projects";
+
+export function ProjectsHead() { return <PageTitle kind="projects" />; }
 
